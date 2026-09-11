6cceb24f026f6a309cf4c1cb5b8938bd17645f52
kadu <0001231422@senaimgaluno.com.br>
2026-09-10 22:06:38 -0300

    alteraçao de estilo css

---
+++ autenticar.php
@@ -0,0 +1,24 @@
+<?php
+
+include 'conexao.php';
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "SELECT * FROM clientes WHERE senha = '$senha' AND email = '$email'";
+
+$resultado = mysqli_query(
+    $conexao,
+    $sql
+);
+
+
+
+if(mysqli_num_rows($resultado) > 0){
+   header("Location: minhas_reservas.php");
+   exit();
+}else{
+   header("Location: login.html");
+exit();
+}
+?>
